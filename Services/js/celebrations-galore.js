2e06f2c1603682c41e3ce50d5d5b731d3b4b1487
Chandan N <chandan2004.n@gmail.com>
2026-06-21 13:10:29 +0530

    not added footer and navbar

--- Services/js/celebrations-galore.js
+++ Services/js/celebrations-galore.js
@@ -185,3 +185,17 @@ statCards.forEach((card, index) => {
     `floatAnim 3s ease-in-out ${index * .3}s infinite`;
 
 });
+
+// image flip effect
+
+const images = document.querySelectorAll('.image-flip-container img');
+
+let current = 0;
+
+setInterval(() => {
+    images[current].classList.remove('active');
+
+    current = (current + 1) % images.length;
+
+    images[current].classList.add('active');
+}, 3000);
